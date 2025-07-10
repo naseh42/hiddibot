1f43438637f23252c262a7384c05b184ac48ef71
naseh42 <naseh42@gmail.com>
2025-07-10 11:39:38 +0330

    Create plans.py

---
+++ plans.py
@@ -0,0 +1,6 @@
+plans = [
+    {"id": "p1", "label": "1 ماهه / 50GB", "duration": 30, "volume": 50, "price": 50000},
+    {"id": "p2", "label": "3 ماهه / 150GB", "duration": 90, "volume": 150, "price": 110000},
+]
+
+trial_plan = {"label": "اشتراک تست", "duration": 1, "volume": 2}
