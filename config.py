ef6ee2a59b21e9a0ff0462f518feffad882f777f
naseh42 <naseh42@gmail.com>
2025-07-10 11:38:16 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+BOT_TOKEN = "توکن_ربات"
+ADMIN_ID = 123456789
+MERCHANT_ID = "کد_زرین‌پال"
+CALLBACK_URL = "https://yourdomain.com/callback"
+HIDDIFY_API_URL = "https://panel.domain.com/api/v1"
+HIDDIFY_TOKEN = "توکن_هیدیفای"
